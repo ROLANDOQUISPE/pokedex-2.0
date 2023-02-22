d96fc1af553b4aec919c773a96fac8b81c952131
rolando <rolando.quispe@alumnos.cmwt.cl>
2023-02-22 16:21:00 -0600

    puse una explicacion

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 console.log("pokeapi");
 
 const buscar = async (id) => {
+    // la url del pokemon poniendo el id del usuario busco para que traiga ese pokemon 
     var pokeurl = `https://pokeapi.co/api/v2/pokemon-species/${id}/`
 
-    //url de la imagen
+    //url de la imagen poniendo el id del usuario busco para que traiga ese pokemon
     var pokefoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
 
-
+    // lo convierto a json
     const pokeurlApi = await fetch(pokeurl)
     const pokeurlApiJson = await pokeurlApi.json()
+
+    // lo unico que pude sacar para las tarjetas
     const pokenombre = await pokeurlApiJson.name
     const pokeid =await pokeurlApiJson.id
     
@@ -17,13 +20,20 @@ const buscar = async (id) => {
     //const poketype = await pokeurlApiJson.types
     // console.log(poketype);
 
+
+
         let cartasDiv = document.getElementById("contenedorCartas")
 
+
+   //limpia el contenedor     
     while (cartasDiv.firstChild) {
         cartasDiv.removeChild(cartasDiv.firstChild)
     }
 
+    //imprimo la url de la imagen para revisar a donde va
 console.log(pokefoto);
+
+     // la estrutura de la tarjeta por motivo de no poder explorar tuve que usar solo lo que podia sacar de LA API
     cartasDiv.innerHTML += ` <div class="card mb-2 col-sm-12 col-md-6 col-lg-4" style="width: 18rem;">
         <img class="card-img-top" src= ${pokefoto}alt=${pokenombre}>
         <div class="card-body">
@@ -36,6 +46,7 @@ console.log(pokefoto);
         
         `
 }
+//busca por el id que el usuario puso 
 function buscapokemon() {
     const id_poke = document.getElementById("id_poke")
     var idPOKE = id_poke.value
